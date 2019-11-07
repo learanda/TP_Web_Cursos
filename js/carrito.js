d5187f8688bc5277c6b4c86d8125cf82ebba949b
learanda <learanda@hotmail.com.ar>
2019-11-07 00:25:06 -0300

    Agrego sessionStorage

---
+++ js/carrito.js
@@ -0,0 +1,21 @@
+window.onload=function(){
+	if(sessionStorage.contador != undefined){
+		document.getElementById("numcarrito").innerHTML = sessionStorage.contador
+	}
+}
+
+function comprar(){
+    // Si existe contador se incrementa y sino se inicializa
+    if (sessionStorage.contador) {
+        // Se convierte el contador a entero porque por defecto es una cadena
+        sessionStorage.contador = parseInt(sessionStorage.contador) + 1;
+    } else {
+        sessionStorage.contador = 1;
+    }
+    document.getElementById("numcarrito").innerHTML = sessionStorage.contador;
+}
+
+/*
+function resetearClicks() {
+    localStorage.removeItem("contador");
+}*/
